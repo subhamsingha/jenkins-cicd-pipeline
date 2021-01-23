54ff4242d6c2aacf36eb25c57e35b80ccd2eb3c4
subham <singhasubham191@gmail.com>
2021-01-23 10:23:38 +0530

    adding secret key

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,13 +4,13 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@34.229.19.184:/tmp/.auth
+scp -i ${newKey} /tmp/.auth prod-user@34.229.19.184:/tmp/.auth
 
 
 #transfer the publish.sh script to remote machine
-scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@34.229.19.184:/tmp/publish.sh
+scp -i ${newKey} ./jenkins/deploy/publish.sh prod-user@34.229.19.184:/tmp/publish.sh
 
 #run the publish.sh script to the deployment machine
 #this will deploy the container to deployment machine from host machine i.e jenkins machine
 
-ssh -i /opt/prod prod-user@34.229.19.184 "/tmp/publish.sh"
+ssh -i ${newKey} prod-user@34.229.19.184 "/tmp/publish.sh"
